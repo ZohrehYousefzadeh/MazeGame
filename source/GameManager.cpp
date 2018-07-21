b718fdf3e05aa8fa1c90b86fdc2d713521bd066a
ZohrehYousefzadeh <40006791+ZohrehYousefzadeh@users.noreply.github.com>
2018-07-21 13:35:43 -0400

    Create GameManager.cpp

---
+++ source/GameManager.cpp
@@ -0,0 +1,18 @@
+#include "../include/GameManager.h"
+
+using namespace Maze;
+using namespace Maze::InputManager;
+using namespace Maze::TableManager;
+
+namespace Maze
+{
+	void Game::update_table(int start)
+	{
+	}
+	void Game::find_way()
+	{
+	}
+	void Game::run()
+	{
+	}
+}
